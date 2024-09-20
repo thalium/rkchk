2dd2deada39afce4325e9dc4df45d29ab85d69c2
Antoine Doglioli <antoine.doglioli@gmail.com>
2024-09-20 16:57:10 +0200

    Added function integrity check

--- README.md
+++ README.md
@@ -15,8 +15,22 @@ Many rootkit hide themselves from the module list (/proc/modules, lsmod ...) by 
 The module hook (using fprobe) many commonly used by LKM rootkit linux API function.
 The callback function get the caller ip and using `kallsyms_module` check if the caller is a module or not, and if so check if it's name is in the kernel's module list. An absent name is highly suspicious.
 
+## Function integrity
 
-# Rootkit checked
+Many rootkit act on the system by hooking multiple function such as `ip_rcv` to hide communication and open backdoor (listen for magic packet for example). 
+
+Other commonly hooked function is `sys_kill`, often used as a way of communication with the module assigning to some signal number specials actions such as giving root to the user or unhiding himself.
+
+Some rootkit have their own framework to hook thoses function (for example `khook` for Reptile) and work by modifying the code of the hooked function to modify their flow of execution.
+
+This check work by reading the text of multiple commonly hooked function (see the list given after) as loading and regulary checking the integrity of the function.
+
+This check don't work if the rootkit is already present at the loading of the module.
+
+Checked functions :
+- `ip_rcv`
+
+# Rootkit tested and detected
 
 Reptile : https://github.com/f0rb1dd3n/Reptile
 Diamorphine : https://github.com/m0nad/Diamorphine
--- rootkit_detection.rs
+++ rootkit_detection.rs
@@ -4,9 +4,17 @@
 
 use core::ffi::c_char;
 use core::result::Result::Ok;
+use core::slice;
+use core::str;
 
+use kernel::sync::Arc;
+
+use core::mem;
+use kernel::c_str;
+use kernel::miscdev::Registration;
 use kernel::error::Result;
 use kernel::bindings;
+use kernel::file;
 use kernel::module::is_module;
 use kernel::str::CStr;
 
@@ -14,9 +22,12 @@ use kernel::prelude::*;
 use kernel::fprobe;
 use kernel::module;
 
+/// The maximum number of byte we saved for a given function
+const MAX_SAVED_SIZE : usize = 4096;
+
 
 module! {
-    type: Probes,
+    type: RootkitDetection,
     name: "rootkit_detection",
     author: "Rust for Linux Contributors",
     description: "Rust rootkit detection module",
@@ -45,7 +56,7 @@ fn check_module_consistency(regs : &bindings::pt_regs) -> Result {
     let mut offset : u64 = 0;
     let mut _symbolsize : u64 = 0;
 
-    let (modname, symbol) = module::symbols_lookup(pip, &mut offset, &mut _symbolsize)?;
+    let (modname, symbol) = module::symbols_lookup_address(pip, &mut offset, &mut _symbolsize)?;
 
     if let Some(name) = modname {
         pr_info!("Module source : {}\n", CStr::from_bytes_with_nul(name.as_slice())?);
@@ -118,18 +129,103 @@ impl fprobe::FprobeOperations for KallsymsLookupNameProbe {
     }
 }
 
+struct FunctionIntegrity {
+    saved_function : Vec<(String, Vec<u8>)>,
+}
 
+impl FunctionIntegrity {
+
+    fn init() -> Result<Self> {
+        let mut fct_integ = FunctionIntegrity {
+            saved_function: Vec::new()
+        };
+
+        fct_integ.save_function(c_str!("ip_rcv"))?;
+
+        Ok(fct_integ)
+    }
+
+    /// Return the bytes of the function `name`
+    /// Return at max MAX_SAVED_SIZE first bytes
+    fn get_function_as_bytes(&self, name : &CStr) -> Result<Vec<u8>> {
+        let addr = module::symbols_lookup_name(name);
+
+        if addr == 0 as u64 {
+            return Err(EINVAL);
+        }
+
+        let ( mut symbolsize, offset) = module::symbols_lookup_size_offset(addr);
+
+        if offset != 0 {
+            return Err(EINVAL);
+        }
+
+        static_assert!(((MAX_SAVED_SIZE * mem::size_of::<u8>()) as u128) < (isize::MAX as u128) );
+
+        if symbolsize > MAX_SAVED_SIZE {
+            symbolsize = MAX_SAVED_SIZE;
+        }
+
+        // SAFETY : 
+        // 1- The object we manipulate is code so it should not be deallocated, except if the module we look at is unloaded (TODO : find a way to ensure that the module is not unloaded)
+        // 2- addr is non null and aligned (because we ask for 1 byte data)
+        // 3- from addr to addr+symbolsize the memory is valid and readable (in the kernel or a module)
+        // 4- The slice is non mutable so it will not be mutated
+        // 5- symbolsize <= MAX_SAVED_SIZE < mem::size_of::<u8>() * isize::MAX
+        let tab : &[u8] = unsafe { slice::from_raw_parts(addr as *const u8, symbolsize as usize)};
+
+        let mut buffer = Vec::try_with_capacity(symbolsize)?;
+        buffer.try_extend_from_slice(tab)?;
+
+        Ok(buffer)
+    }
+
+    // Save the function in the struct's `saved_function` field
+    fn save_function(&mut self, name : &CStr) -> Result {
+        let buffer = self.get_function_as_bytes(name)?;
+
+        let mut name_buf : Vec<u8> = Vec::try_with_capacity(name.len())?;
+        name_buf.try_extend_from_slice(name.as_bytes_with_nul())?;
+        let name = match String::from_utf8(name_buf) {
+            Ok(name) => name,
+            Err(_) => return Err(EINVAL)
+        };
+    
+        self.saved_function.try_push((name, buffer))?;
+
+        Ok(())
+    }
+    fn check_functions<'a>(&'a self) -> Result<Option<&'a String>> {
+        for (name, bytes) in &self.saved_function {
+            let new_bytes = self.get_function_as_bytes(CStr::from_bytes_with_nul(name.as_bytes())?)?;
+            if new_bytes != *bytes {
+                return Ok(Some(name));
+            }
+       }
+       Ok(None)
+    }
+
+}
+
+#[vtable]
+impl file::Operations for FunctionIntegrity {
+    type OpenData = Arc<Self>;
+    fn open(context: &Self::OpenData, _file: &file::File) -> Result<Self::Data> {
+        if let Some(fct) = context.check_functions()? {
+            pr_alert!("Function {} is hooked\n", fct);
+        }
+        Ok(())
+    }
+}
 struct Probes {
     _usermodehelper_probe : Pin<Box<fprobe::Fprobe<UsermodehelperProbe>>>,
     _init_module_probe : Pin<Box<fprobe::Fprobe<SysInitModuleProbe>>>,
     _commit_creds_probe : Pin<Box<fprobe::Fprobe<CommitCredsProbe>>>,
-    _kallsyms_lookup_name_probe : Pin<Box<fprobe::Fprobe<KallsymsLookupNameProbe>>>
+    _kallsyms_lookup_name_probe : Pin<Box<fprobe::Fprobe<KallsymsLookupNameProbe>>>,
 }
 
-impl kernel::Module for Probes {
-    fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
-        pr_info!("Rootkit detection written in Rust\n");
-
+impl Probes {
+    fn init() -> Result<Self> {
         pr_info!("Registering probes\n");
 
         let _usermodehelper_probe = fprobe::Fprobe::new_pinned("call_usermodehelper", None)?;
@@ -140,12 +236,14 @@ impl kernel::Module for Probes {
 
         let _kallsyms_lookup_name_probe = fprobe::Fprobe::new_pinned("kallsyms_lookup_name", None)?;
 
-        Ok(Probes { 
-            _usermodehelper_probe,
-            _init_module_probe,
-            _commit_creds_probe,
-            _kallsyms_lookup_name_probe
-        })
+        let probes = Probes { 
+                                    _usermodehelper_probe,
+                                    _init_module_probe,
+                                    _commit_creds_probe,
+                                    _kallsyms_lookup_name_probe,
+                                };
+
+        Ok(probes)
     }
 }
 
@@ -154,3 +252,35 @@ impl Drop for Probes {
         pr_info!("Rootkit detection (exit)\n");
     }
 }
+
+struct RootkitDetection {
+    _registration : Pin<Box<Registration<FunctionIntegrity>>>,
+    _probe : Probes,
+    _function_integrity : Arc<FunctionIntegrity>,
+}
+
+impl kernel::Module for RootkitDetection {
+    fn init(name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
+        pr_info!("Rootkit detection written in Rust\n");
+
+        pr_info!("Registering the device\n");
+        
+        let _probe = Probes::init()?;
+
+        let _function_integrity = Arc::try_new(FunctionIntegrity::init()?)?;
+
+        let _registration = kernel::miscdev::Registration::<FunctionIntegrity>::new_pinned(fmt!("{name}"), _function_integrity.clone())?;
+    
+        Ok(RootkitDetection {
+            _registration,
+            _probe,
+            _function_integrity,
+        })
+    }
+
+    
+}
+
+
+
+
